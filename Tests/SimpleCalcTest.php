381edf7959eb530508bf0bc4ad51b223531c0e41
wojtek25 <wojciech.chec@gmail.com>
2013-06-11 10:31:37 +0200

    adding tests

--- Tests/SimpleCalcTest.php
+++ Tests/SimpleCalcTest.php
@@ -64,10 +64,29 @@ class SimpleCalcTest extends \PHPUnit_Framework_TestCase
         SimpleCalc::divide(30, 0);
     }
 
+
     public function testMaxFromArray()
     {
         $this->assertEquals(6, SimpleCalc::maxFromArray(array(1, 6, 2)));
         $this->assertEquals(1, SimpleCalc::maxFromArray(Array(-2,-3,1,0,-10)));
         $this->assertEquals(2, SimpleCalc::maxFromArray(Array(2)));
     }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @dataProvider getAverageData
+     */
+    public function testAverage($result, $a)
+    {
+        SimpleCalc::average($result, $a);
+    }
+    public function getAverageData()
+    {
+        return array(
+            array(3.5, array(2, 5)),
+            array(4.5, array(5, 4)),
+            array(2.5, array(2, 3)),
+        );
+    }
+
 }
